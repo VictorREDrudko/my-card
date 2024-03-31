1b3825aa1d485dd0c99f7d800c8e56f539ccf566
Victor RED <viktor_RED_rudko@mail.ru>
2024-03-31 22:36:38 +0300

    feat: add title, favicon, font and container

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Card = () => {
+  return (
+    <StyledCard>
+      
+    </StyledCard>
+  )
+}
+
+const StyledCard = styled.div`
+  background-color: #FFFFFF;
+  width: 300px;
+  height: 350px;
+  border-radius: 15px;
+  box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.1);
+`
---
+++ src/styles/Global.styled.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  /* Зануляем отступы */
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  /* Убираем подчеркивания для ссылок */
+  a {
+    text-decoration: none;
+  }
+
+  /* Для списков убираем маркеры */
+  ul {
+    list-style: none;
+  }
+
+  /* Для кнопок убираем серый background */
+  button {
+    background-color: unset;
+    border: none;
+  }
+`
